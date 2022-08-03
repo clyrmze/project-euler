d4e77352d5a85f4322e1822b9590d6d9cead886c
yogii <47711835+clyrmze@users.noreply.github.com>
2022-08-03 15:57:29 +0530

    Add p17

--- Solutions/p0017.py
+++ Solutions/p0017.py
@@ -1,3 +1,9 @@
+"""If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+"""
+
 single = {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',
 6:'six',7:'seven',8:'eight',9:'nine'}
 
